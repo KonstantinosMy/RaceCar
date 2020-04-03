cc12c78b3d6d6bbad9222039a79bda0ff09d77ae
Konstantinos Mylonas <konstantinosmy@hotmail.com>
2020-04-03 13:47:53 +0300

    Initial commit

---
+++ Assets/winCON.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winCON : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        { 
+
+            SceneManager.LoadScene("Demo 3");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "red")
+        {
+            Debug.Log("RED WINS");
+            SceneManager.LoadScene("Demo 3");
+        }
+        else if (other.tag == "blue")
+        {
+            Debug.Log("BLUE WINS");
+            SceneManager.LoadScene("Demo 3");
+        }
+    }
+}
